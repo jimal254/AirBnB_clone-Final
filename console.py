5c8a732647978dc7fddcf49867e926af29a175b8
jimmy <jimmymwas@gmail.com>
2024-02-10 22:26:53 -0800

    AirBnB Clone - The Console

--- console.py
+++ console.py
@@ -1,47 +1,24 @@
-#!/usr/bin/env python3
-"""
-Console module for HBNB command interpreter.
-"""
+#!/usr/bin/python3
+""" Console module """
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """
-    Command interpreter class.
-    """
-
+    """ HBNB command interpreter """
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """
-        Quit command to exit the program.
-        """
+        """ Quit command to exit the program """
         return True
 
     def do_EOF(self, arg):
-        """
-        Exit the program with Ctrl+D (EOF).
-        """
-        print()
+        """ Exit program at End of File (EOF) """
+        print("")
         return True
 
     def emptyline(self):
-        """
-        Empty line does not execute anything.
-        """
+        """ Do nothing on empty line """
         pass
-
-    def help_quit(self):
-        """
-        Help message for the quit command.
-        """
-        print("Quit command to exit the program")
-
-    def help_EOF(self):
-        """
-        Help message for the EOF command.
-        """
-        print("Exit the program with Ctrl+D (EOF)")
 
 
 if __name__ == '__main__':
